754de389dd2d7c4a36fde395a3ab7829df8dfd2e
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2015-09-07 10:12:42 +0200

    Add comments

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -26,12 +26,21 @@ var preview = {
   }
 };
 
-var homesCheck = document.getElementById('homes-check');
 var roomsCheck = document.getElementById('rooms-check');
+var homesCheck = document.getElementById('homes-check');
 
+/*
+Subscribes to the change eventlisteners and on change filters the homes.
+*/
 homesCheck.addEventListener('change', function (e) { filterHomes(homeCollection) });
 roomsCheck.addEventListener('change', function (e) { filterHomes(homeCollection) });
 
+/*
+Calls getPreview via the home object, 
+after which it selects the home and updates the preview.
+@param {Event} event
+@param {Object} home
+*/
 function openHome(event, home) {
   getPreview(home)
   .then(function (value) {
@@ -40,6 +49,10 @@ function openHome(event, home) {
   });
 }
 
+/*
+Sets the element which matches _home.url to selected and deselects every other element.
+@param {Object} _home
+*/
 function selectHome(_home) {
   _.map(homeCollection, function (home) {
     if (home.home.url == _home.url) {
@@ -51,23 +64,32 @@ function selectHome(_home) {
   });
 }
 
+/*
+Iterates over the home collection to either show or hide the element.
+@param {Array} collection
+*/
 function filterHomes(collection) {
+  console.log(collection);
   _.map(collection, function (item) {
-    item.element.hidden = !showItem(item);
+    item.element.hidden = !shouldShow(item);
   });
 }
 
 function updatePreview(item, home) {
   _.assign(preview.data, home, item);
-  
-  console.log(preview.data);
-  
   _.map(preview.dom, function (element, key) {
     element.innerHTML = preview.data[key];
   });
 }
 
-function showItem(homeItem) {
+/*
+Returns either true or false for whether the home type matches the the check boxes.
+For instance, if homeItem.isRoom and roomsCheck.checked then return true is returned.
+If neither check is checked, the function will return true.
+@param {Object} homeItem
+@return {Bool}
+*/
+function shouldShow(homeItem) {
   if (!roomsCheck.checked && !homesCheck.checked) { return true; }
 
   if (homeItem.home.isRoom) { return roomsCheck.checked; }
@@ -76,6 +98,11 @@ function showItem(homeItem) {
   return true;
 }
 
+/*
+Adds the collection's elements to DOM.
+@param {Array} collection
+@return {Promise}
+*/
 function addToDOM(collection) {
   return new Promise(function (resolve, reject) {
     homeContainer.innerHTML = _.map(collection, function (item) {
@@ -95,6 +122,12 @@ function addToDOM(collection) {
   });
 }
 
+/*
+Iterates over the homes and creates elements populated with their data.
+Returns a homeCollection containing all elements for the home list.
+@param {Array} homes
+@return {Promise}
+*/
 function createHomeElements(homes) {
   return new Promise(function (resolve, reject) {
     homeCollection = _.map(homes, function (home, index) {
@@ -116,9 +149,20 @@ function createHomeElements(homes) {
   });
 }
 
+/*
+Fetches recent homes from serves
+@return {Promise}
+*/
 function getHomes() { return _http.get('/api/recent') };
 
+/*
+Analyzes and classifies the homes,
+tagging it as home if it is believed to be a home based on the title.
+@param {Array} homes
+@return {Promise}
+*/
 function analyze(homes) {
+  console.log(homes);
   return new Promise(function (resolve, reject) {
     _homes = homes;
 
@@ -131,10 +175,18 @@ function analyze(homes) {
   });
 }
 
+/*
+Fetches the input home item from server.
+@param {Object} homeItem
+@return {Promise}
+*/
 function getPreview(homeItem) {
   return _http.put('/api/preview', { url: homeItem.url });
 }
 
+/*
+Gets homes and sets everything up.
+*/
 getHomes()
   .then(analyze)
   .then(createHomeElements)
