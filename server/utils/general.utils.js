50280ae04190eb9866315d2d65212f3b672f47f3
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-01-23 23:28:38 +0100

    Add 'objectify' method to utils

--- server/utils/general.utils.js
+++ server/utils/general.utils.js
@@ -5,6 +5,7 @@ var request = require('request');
 var Promise = require('bluebird');
 var moment = require('moment');
 var Browser = require('zombie');
+var DataObjectParser = require('dataobject-parser')
 
 /**
  * Returns true or false for all properties of *target* matches those of *source*.
@@ -239,6 +240,37 @@ function getTel(url) {
   });
 }
 
+/**
+ * Returns a new object where property names
+ * with dots are converted into nested objects and arrays.
+ * 
+ * Example: { 'prop.sub': 'value' } -> { prop: { sub: value } }
+ * 
+ * @param {Array|Object} sqlArray
+ * @return {Array|Object}
+ */
+function objectify(sqlArray) {
+  // Ensure it's an array
+  var isObj;
+  if (!_.isArray(sqlArray)) {
+    sqlArray = [ sqlArray ];
+    isObj = true;
+  }
+  
+  var arr = _.map(sqlArray, function (sqlObj) {
+    var d = new DataObjectParser();
+    
+    // Get all values
+    _.map(sqlObj, function (value, key) {
+      d.set(key, value);
+    });
+    
+    return d.data();
+  });
+  
+  return isObj ? _.first(arr) : arr;
+}
+
 module.exports = {
   getPage: getPage,
   getManyPages: getManyPages,
@@ -248,5 +280,6 @@ module.exports = {
   nextMonth: nextMonth,
   getClosestDate: getClosestDate,
   hasTel: hasTel,
-  getTel: getTel
+  getTel: getTel,
+  objectify: objectify
 };
