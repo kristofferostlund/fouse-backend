07ec163ea8a5a4f6245119fb0ab77a749ffc571a
Kristoffer Ostlund <kristoffer.ostlund@gmail.com>
2016-08-09 20:00:00 +0200

    Refactor api

---
+++ server/api/api.homeItems.js
@@ -0,0 +1,44 @@
+'use strict'
+
+var _ = require('lodash');
+var Promise = require('bluebird');
+
+var utils = require('./../utils/utils');
+var auth = require('./../services/auth.service');
+var HomeItem = require('./../models/homeItem/homeItem.model');
+
+/**
+ * Route GET '/api/home-items'
+ */
+function listHomes(req, res) {
+  // Get the page number from the query params
+  var page = _.get(req, 'query.page') || 1;
+
+  // *page* must be creater than zero, otherwise defaults to 1
+  page = page >= 1 ? page : 1;
+
+  // Get the page size/limit from the query params
+  var limit = _.get(req, 'query.limit') || 20;
+
+  //  Limut must be greater than zero, otherwise defaults to 20
+  limit = limit >= 1 ? limit : 20 ;
+
+  // Get the number of items to skip
+  var _skip = (page - 1 ) * limit;
+
+  HomeItem.find({ isDisabled: { $ne: true }, active: true })
+  .skip(_skip)
+  .limit(limit)
+  .sort({ $natural: -1 })
+  .exec()
+  .then(function (homeItems) {
+    res.status(200).json(homeItems);
+  })
+  .catch(function (err) {
+    utils.handleError(res, err);
+  });
+}
+
+module.exports = {
+  listHomes: listHomes,
+}
--- server/api/index.js
+++ server/api/index.js
@@ -9,15 +9,15 @@ var auth = require('./../services/auth.service');
 
 router.use(cors());
 
-router.post('/authenticate', controller.login);
+router.post('/authenticate', controller.user.login);
 
-router.post('/users', controller.createUser);
-router.get('/users', auth.isAuthenticated(), controller.listUsers);
-router.get('/users/me', auth.isAuthenticated(), controller.me);
-router.get('/users/:id', auth.isAuthenticated(), controller.getUser);
-router.put('/users/:id', auth.isAuthenticated(), controller.updateUser);
-router.put('/users/:id/password', auth.isAuthenticated(), controller.updateUserPassword);
+router.post('/users', controller.user.createUser);
+router.get('/users', auth.isAuthenticated(), controller.user.listUsers);
+router.get('/users/me', auth.isAuthenticated(), controller.user.me);
+router.get('/users/:id', auth.isAuthenticated(), controller.user.getUser);
+router.put('/users/:id', auth.isAuthenticated(), controller.user.updateUser);
+router.put('/users/:id/password', auth.isAuthenticated(), controller.user.updateUserPassword);
 
-router.get('/home-items', auth.isAuthenticated(), controller.listHomes);
+router.get('/home-items', auth.isAuthenticated(), controller.homeItems.listHomes);
 
 module.exports = router;
